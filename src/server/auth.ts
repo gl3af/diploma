e02a4726dbc7c6e9ea5b61d2c03b6c5d976bcfbf
gl3af <ybaranovnikita2002@gmail.com>
2023-12-27 12:30:56 +0300

    Стартуем

---
+++ src/server/auth.ts
@@ -0,0 +1,41 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import {
+  getServerSession,
+  type DefaultSession,
+  type NextAuthOptions,
+} from "next-auth";
+import VkProvider from "next-auth/providers/vk";
+
+import { env } from "@/env";
+import { db } from "@/server/db";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    } & DefaultSession["user"];
+  }
+}
+
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
+  },
+  adapter: PrismaAdapter(db),
+  providers: [
+    VkProvider({
+      clientId: env.VK_CLIENT_ID,
+      clientSecret: env.VK_CLIENT_SECRET,
+    }),
+  ],
+};
+
+export const getServerAuthSession = () => getServerSession(authOptions);
