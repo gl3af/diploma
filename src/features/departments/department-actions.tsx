17d6eeeac72d66cbbaac39d3e666a7eb43ed3d6c
gl3af <ybaranovnikita2002@gmail.com>
2024-05-22 16:02:43 +0300

    hotfix: действия над отделом

--- src/features/departments/department-actions.tsx
+++ src/features/departments/department-actions.tsx
@@ -1,8 +1,15 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
 import { Box } from "@/shared/ui";
 
 import { DeleteDepartmentModal, EditDepartmentModal } from "./modals";
 
 export function DepartmentActions({ id }: { id: number }) {
+  const { data } = useSession();
+  if (data?.user.role !== "admin") return null;
+
   return (
     <Box className="flex items-center gap-2">
       <EditDepartmentModal id={id} />
